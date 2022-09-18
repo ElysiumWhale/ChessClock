c131a17ef952fb3d94126da53c39dbf336bdf5f6
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-09-19 01:27:30 +0400

    Replace TimeView

--- ChessClock/ChessClock/View/Clock/ClockView.swift
+++ ChessClock/ChessClock/View/Clock/ClockView.swift
@@ -23,26 +23,5 @@ struct ClockView_Previews: PreviewProvider {
         ClockView()
     }
 }
-
-struct TimeView: View {
-    @ObservedObject var timeStamp: TimeStamp
-    var color: Color = SettingsManager.shared.workColor
-    
-    var body: some View {
-        ZStack {
-            color
-            VStack(alignment:.center) {
-                HStack {
-                    Spacer()
-                    Text("\(timeStamp.hours) :")
-                        .timeStyle()
-                    Text("\(timeStamp.minutes) :")
-                        .timeStyle()
-                    Text("\(timeStamp.seconds)")
-                        .timeStyle()
-                    Spacer()
-                }
-            }
-        }
     }
 }
---
+++ ChessClock/ChessClock/View/Clock/TimeView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct TimeView: View {
+    @ObservedObject var timeStamp: TimeStamp
+
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("\(timeStamp.hours) :")
+                .timeStyle()
+            Text("\(timeStamp.minutes) :")
+                .timeStyle()
+            Text("\(timeStamp.seconds)")
+                .timeStyle()
+            Spacer()
+        }
+    }
+}
+
+
+struct TimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeView(timeStamp: .init(timeType: .work))
+    }
+}
+
+extension Text {
+    func timeStyle() -> Text {
+        font(.system(.largeTitle, design: .rounded)).fontWeight(.bold)
+    }
+}
