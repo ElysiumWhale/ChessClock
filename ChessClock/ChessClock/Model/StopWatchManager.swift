999cf667d6f68b017b3e186ec40e2c47ea662188
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-09-21 03:43:13 +0400

    Fix indentations and code style

--- ChessClock/ChessClock/Model/StopWatchManager.swift
+++ ChessClock/ChessClock/Model/StopWatchManager.swift
@@ -12,14 +12,19 @@ enum TimerActive {
     case work
 }
 
-class StopWatchManager: ObservableObject {
+final class StopWatchManager: ObservableObject {
 
-    @Published var restTime: TimeStamp = TimeStamp(timeType: .rest)
-    @Published var workTime: TimeStamp = TimeStamp(timeType: .work)
+    @Published
+    var restTime: TimeStamp = TimeStamp(timeType: .rest)
+    @Published
+    var workTime: TimeStamp = TimeStamp(timeType: .work)
 
-    @Published var state: StopWatchState = .stopped
-    @Published var timerActive: TimerActive = .none
+    @Published
+    var state: StopWatchState = .stopped
+    @Published
+    var timerActive: TimerActive = .none
 
+    @Published
     var timeTries: [TimeTry] = [TimeTry]()
     var currentTry: TimeTry?
 
@@ -53,9 +58,6 @@ class StopWatchManager: ObservableObject {
         workTimer.invalidate()
         restTimer.invalidate()
 
-        // todo
-        // currentTry!.intervals.append(<#T##newElement: TimeStamp##TimeStamp#>)
-        // timeTries.append(currentTry!)
         restTime = .init(timeType: .rest)
         workTime = .init(timeType: .work)
         currentTry = nil
@@ -65,20 +67,20 @@ class StopWatchManager: ObservableObject {
         switch state {
         case .stopped: return
         case .running:
-            guard !(sender == timerActive) else {
+            guard sender != timerActive else {
                 return
             }
             fallthrough
         case .paused:
-            if sender == .rest {
-                if state == .paused { state = .running }
-                //TODO: intervals append
+            switch sender {
+            case .none: return
+            case .rest:
+                state = .running
                 workTimer.invalidate()
                 restTimer = setupTimer(.rest)
                 timerActive = .rest
-            } else if sender == .work {
-                if state == .paused { state = .running }
-                //TODO: intervals append
+            case .work:
+                state = .running
                 restTimer.invalidate()
                 workTimer = setupTimer(.work)
                 timerActive = .work
@@ -89,20 +91,24 @@ class StopWatchManager: ObservableObject {
     func setupTimer(_ timeType: timeType) -> Timer {
         switch timeType {
         case .rest:
-            return Timer.scheduledTimer(withTimeInterval: 1, repeats: true)
-            { [self] timer in
+            return Timer.scheduledTimer(withTimeInterval: 1,
+                                        repeats: true) { [self] timer in
                 if restTime.seconds == 59 {
                     restTime.minutes += 1
                     restTime.seconds = 0
-                } else { restTime.seconds += 1 }
+                } else {
+                    restTime.seconds += 1
+                }
             }
         case .work:
-            return Timer.scheduledTimer(withTimeInterval: 1, repeats: true)
-            { [self] timer in
+            return Timer.scheduledTimer(withTimeInterval: 1,
+                                        repeats: true) { [self] timer in
                 if workTime.seconds == 59 {
                     workTime.minutes += 1
                     workTime.seconds = 0
-                } else { workTime.seconds += 1 }
+                } else {
+                    workTime.seconds += 1
+                }
             }
         }
     }
