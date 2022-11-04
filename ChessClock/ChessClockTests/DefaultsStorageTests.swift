509d069c9c0d7c34baaa45a400ea1536f493eb08
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-11-05 02:29:36 +0400

    Add DefaultsStorage tests

---
+++ ChessClock/ChessClockTests/DefaultsStorageTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import ChessClock
+
+final class DefaultsStorageTests: XCTestCase {
+
+    enum TestKeys: String, CodingKey {
+        case test
+    }
+
+    private let testContainer = DefaultsContainer(name: "ChessClock.Test")
+
+    func testDefaultsStorageSaveRetrieveClear() {
+        let expectation = 100
+        let testStorage = DefaultsStorage<TestKeys>(
+            container: testContainer
+        )
+
+        testStorage.save(value: expectation, key: .test)
+        let retrieved: Int? = testStorage.retrieve(key: .test)
+        XCTAssertEqual(retrieved, expectation)
+        testStorage.clearAll()
+        let nilRetrieved: Int? = testStorage.retrieve(key: .test)
+        XCTAssertNil(nilRetrieved)
+    }
+}
