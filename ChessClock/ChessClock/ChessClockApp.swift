bab392c2cfccd29a37a85d7d0a9352fdcc000969
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-11-23 03:07:05 +0400

    Implement background mode

--- ChessClock/ChessClock/ChessClockApp.swift
+++ ChessClock/ChessClock/ChessClockApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct ChessClockApp: App {
-    //@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
 
     private let mainDependency = ChessClockDependency(
         stopwatchService: StopwatchService(),
@@ -12,6 +12,18 @@ struct ChessClockApp: App {
     var body: some Scene {
         WindowGroup {
             ChessClockView(dependency: mainDependency)
+        }
+        .onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .active:
+                mainDependency.stopwatchService.revive()
+            case .background:
+                mainDependency.stopwatchService.suspend()
+            case .inactive:
+                break
+            @unknown default:
+                break
+            }
         }
     }
 }
