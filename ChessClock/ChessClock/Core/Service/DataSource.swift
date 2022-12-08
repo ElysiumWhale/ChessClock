535aa5e449a3d5046fc67c0808d0c76dcae3f811
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-12-09 03:47:55 +0400

    Add data source

---
+++ ChessClock/ChessClock/Core/Service/DataSource.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+struct TimeTry: Codable, Identifiable {
+    let id: UUID
+    let startDate: Date
+    let intervals: [TimeStamp]
+}
+
+protocol IDataSource: ObservableObject {
+    var models: [TimeTry] { get }
+
+    func remove(at offsets: IndexSet)
+}
+
+final class DataSource: IDataSource, ObservableObject {
+    enum ModelsKeys: String {
+        case savedModels
+    }
+
+    private let storage: any IStorage<ModelsKeys>
+
+    @Published
+    var models: [TimeTry]
+
+    init(
+        storage: any IStorage<ModelsKeys> = DefaultsStorage<ModelsKeys>()
+    ) {
+        self.storage = storage
+        self.models = storage.retrieve(key: .savedModels) ?? []
+    }
+
+    func remove(at offsets: IndexSet) {
+        models.remove(atOffsets: offsets)
+        storage.save(
+            models,
+            key: .savedModels
+        )
+    }
+}
+
+// MARK: - IStopwatchDelegate
+extension DataSource: IStopwatchDelegate {
+    func didFinishCountingModel(_ model: TimeCountingModel) {
+        models.append(model.asTimeTry)
+        storage.save(
+            models,
+            key: .savedModels
+        )
+    }
+}
+
+// MARK: - Converting
+private extension TimeCountingModel {
+    var asTimeTry: TimeTry {
+        TimeTry(id: id, startDate: startDate, intervals: intervals)
+    }
+}
